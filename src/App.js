6ab5b41c835ed95f783518b6eaf7c04fdcf09bbd
Reactdv <jerllylara12@gmail.com>
2022-08-04 23:08:52 +0800

    initialized

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+   import React from 'react'
+   import './App.css'
+   import { Nav } from "./components"
+   import {} from "./pages"
+
+
+    const App = () => {
+     return (
+       <div>
+         <div>
+            <Nav />
+         </div>
+       </div>
+      
+     )
+   }
+
+   export default App
+   
---
+++ src/pages/whatGp3/WhatGp3.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WhatGp3 = () => {
+  return (
+    <div>WhatGp3</div>
+  )
+}
+
+export default WhatGp3
